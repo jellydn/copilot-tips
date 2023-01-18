8eec3c1957d7ac475a6c4545c1ba648f1eb53a98
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-19 00:23:29 +0800

    feat: add short videos

---
+++ src/components/Player.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let videoId;
+	export let title;
+	const thumbnail = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+</script>
+
+<p>{title}</p>
+
+<lite-youtube videoid={videoId} style={`background-image: url(${thumbnail});`}>
+	<a href={`https://youtube.com/watch?v=${videoId}`} class="lty-playbtn" {title}>
+		<span class="lyt-visually-hidden">Play Video: {title}</span>
+	</a>
+</lite-youtube>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Player from '../components/Player.svelte';
+</script>
+
+<h1>Learn how to improve your code with Github Copilot</h1>
+<p>
+	Github Copilot is a powerful tool that can help you write better code and improve your workflow.
+	You can learn more about Github Copilot by visiting the <a
+		href="https://github.com/features/copilot">official webpage</a
+	>. In these short videos, you will quickly learn about the features of Github Copilot and Copilot
+	Labs, and how to use them to their full potential.
+</p>
+
+<section>
+	<h2>Github Copilot</h2>
+	<ol>
+		<li>
+			<Player videoId="gRNo_JOl-9g" title="Github Copilot - Q&A" />
+		</li>
+		<li>
+			<Player videoId="o3qURBllpGM" title="Github Copilot - Second brain" />
+		</li>
+	</ol>
+	<div>
+		<a
+			href="https://marketplace.visualstudio.com/items?itemName=GitHub.copilot"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<button>Install Github Copilot</button>
+		</a>
+	</div>
+</section>
+
+<section>
+	<h2>Github Copilot Labs</h2>
+	<ol>
+		<li>
+			<Player videoId="gb1Jf1Huxs4" title="Github Copilot Labs - Brush up your code" />
+		</li>
+		<li>
+			<Player videoId="oITVx8Ss3Dg" title="Github Copilot Labs - More brush up" />
+		</li>
+	</ol>
+	<div>
+		<a
+			href="https://marketplace.visualstudio.com/items?itemName=GitHub.copilot-labs"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<button>Install Copilot Labs</button>
+		</a>
+	</div>
+</section>
